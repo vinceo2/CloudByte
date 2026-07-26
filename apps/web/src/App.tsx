cd80b3cabd3fc0fbe780862659dc8f162558a7fa
Vince <vincent.otti.ii@gmail.com>
2026-07-25 21:16:46 -0500

    add auth stub

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,8 +2,37 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { checkHealth } from './lib/api';
 import './App.css';
+import { AuthProvider, useAuth } from 'react-oidc-context';
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Q0TsexXQo",
+  client_id: "2379vvdglb5j8hopm9tcjr13a5",
+  redirect_uri: "http://localhost:5173/",
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 function HomePage() {
+
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    auth.stopSilentRenew();
+    auth.removeUser();
+    const clientId = "2379vvdglb5j8hopm9tcjr13a5";
+    const logoutUri = "http://localhost:5173/";
+    const cognitoDomain = "https://us-east-1q0tsexxqo.auth.us-east-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
   const [health, setHealth] = useState<string>('checking...');
 
   useEffect(() => {
@@ -16,11 +45,17 @@ function HomePage() {
     <div className="page">
       <h1>CloudByte</h1>
       <p>Your cloud storage, organized.</p>
+      {auth.isAuthenticated ? <p>Signed in as {auth.user?.profile?.email}</p> : <p>Not signed in</p>}
       <p className="status">API: {health}</p>
       <nav>
         <Link to="/files">Browse files</Link>
       </nav>
+      <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
     </div>
+    </div>
+    
   );
 }
 
@@ -35,13 +70,17 @@ function FilesPage() {
 }
 
 function App() {
+  
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/files" element={<FilesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider {...cognitoAuthConfig}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/files" element={<FilesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
